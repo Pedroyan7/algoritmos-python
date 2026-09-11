2795d193c057a75635bdcd891a98ce5f0463b0e1
pedro yan <pedroyan2007@github.com>
2026-09-11 19:58:15 -0300

    adiciona funcoes de subtraça,multiplicaçao e divisao

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,24 @@ def adicao(num1, num2):
     return soma
 
 print("resultado da soma: ", adicao(16,2))
+
+def subtrair(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+print("resultado da subtraçao: ", subtrair(16,2))
+
+def multiplicar(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+
+print("resultado da multiplicaçao: ", multiplicar(16,2))
+
+def dividir(num1, num2):
+    divisao = num1 / num2
+    return divisa
+
+print("resultado da divisao: ", dividir(16,2))
+
+
+
